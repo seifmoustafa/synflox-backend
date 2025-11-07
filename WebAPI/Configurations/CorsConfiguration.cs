7c992b142d2e0a9516f4ad195879dfcb1058b796
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-11-07 21:28:04 +0200

    Refactor CORS configuration to define a set of allowed origins and enhance origin handling. If an environment variable for allowed origin is set, it is combined with predefined origins, ensuring flexibility and security in CORS policy management.

--- WebAPI/Configurations/CorsConfiguration.cs
+++ WebAPI/Configurations/CorsConfiguration.cs
@@ -6,27 +6,44 @@
 
         public static IServiceCollection AddCorsConfigurationOptions(this IServiceCollection services)
         {
+            // Define allowed origins
+            var allowedOrigins = new[]
+            {
+                "http://localhost",
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "http://localhost:5000",
+                "http://localhost:5001",
+                "http://127.0.0.1:3000",
+                "http://127.0.0.1:5173",
+                "https://synflox-frontend.vercel.app"
+            };
 
             services.AddCors(options =>
             {
                 options.AddPolicy(_policyName, builder =>
                 {
-                    var origin = Environment.GetEnvironmentVariable("ALLOW_ORIGIN");
-                    if (!string.IsNullOrEmpty(origin))
+                    var customOrigin = Environment.GetEnvironmentVariable("ALLOW_ORIGIN");
+                    
+                    if (!string.IsNullOrEmpty(customOrigin))
                     {
-                        builder.WithOrigins(origin)
+                        // If environment variable is set, use it and add to allowed origins
+                        var origins = allowedOrigins.Concat(new[] { customOrigin }).Distinct().ToArray();
+                        builder.WithOrigins(origins)
                                .AllowAnyHeader()
-                               .AllowAnyMethod();
+                               .AllowAnyMethod()
+                               .AllowCredentials();
                     }
                     else
                     {
-                        builder.AllowAnyOrigin()
+                        // Use predefined allowed origins
+                        builder.WithOrigins(allowedOrigins)
                                .AllowAnyHeader()
-                               .AllowAnyMethod();
+                               .AllowAnyMethod()
+                               .AllowCredentials();
                     }
                 });
             });
-
 
             return services;
         }
